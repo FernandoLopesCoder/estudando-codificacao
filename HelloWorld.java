4a64bde124856a2f4f82582852e9ba1f19e5eb2a
Fernando Augusto Fernandes Pereira Lopes <109006217+FernandoLopesCoder@users.noreply.github.com>
2022-07-09 23:47:00 -0300

    criando o arquivo java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * Primeiro código JAVA do recomeço do aprendizado JAVA
+		 */
+
+    System.out.println("Hello World!!!");		
+		
+	}
+
+}
